7d8f4218c4bf716b9c4f400571cdf53cdac3745e
Austin Griffith <austin@ethereum.org>
2023-11-13 09:43:16 -0700

    get working and have celebration with they get contract proof

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -5,7 +5,7 @@ import { hexToBigInt } from "viem";
 import { useAccount } from "wagmi";
 import { isZupassPublicKey, useZuAuth } from "zupass-auth";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 import { notification } from "~~/utils/scaffold-eth";
 import { generateWitness } from "~~/utils/scaffold-eth/pcd";
 import { DEVCONNECT_VALID_EVENT_IDS } from "~~/utils/zupassConstants";
@@ -20,6 +20,8 @@ const fieldsToReveal = {
 
 const Home: NextPage = () => {
   const [verifiedFrontend, setVerifiedFrontend] = useState(false);
+  const [verifiedBackend, setVerifiedBackend] = useState(false);
+  const [verifiedOnChain, setVerifiedOnChain] = useState(false);
   const { authenticate, pcd } = useZuAuth();
   const { address: connectedAddress } = useAccount();
 
@@ -70,12 +72,46 @@ const Home: NextPage = () => {
     );
   };
 
+  const sendPCDToServer = async () => {
+    let response;
+    try {
+      response = await fetch("/api/verify", {
+        method: "POST",
+        body: JSON.stringify({
+          pcd: pcd,
+          address: connectedAddress,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (e) {
+      notification.error(`Error: ${e}`);
+      return;
+    }
+
+    const data = await response.json();
+    setVerifiedBackend(true);
+    notification.success(
+      <>
+        <p className="font-bold m-0">Backend Verified!</p>
+        <p className="m-0">{data?.message}</p>
+      </>,
+    );
+  };
+
   // mintItem verifies the proof on-chain and mints an NFT
   const { writeAsync: mintNFT, isLoading: isMintingNFT } = useScaffoldContractWrite({
     contractName: "YourCollectible",
     functionName: "mintItem",
     // @ts-ignore TODO: fix the type later with readonly fixed length bigInt arrays
     args: [pcd ? generateWitness(JSON.parse(pcd)) : undefined],
+  });
+
+  const { data: yourBalance } = useScaffoldContractRead({
+    contractName: "YourCollectible",
+    functionName: "balanceOf",
+    args: [connectedAddress],
   });
 
   return (
@@ -122,10 +158,19 @@ const Home: NextPage = () => {
                   2. Verify (frontend)
                 </button>
               </div>
+              <div className="tooltip" data-tip="Send the PCD to the server to verify it and execute any action.">
+                <button
+                  className="btn btn-primary w-full"
+                  disabled={!verifiedFrontend || verifiedBackend}
+                  onClick={sendPCDToServer}
+                >
+                  3. Verify (backend) and send ETH
+                </button>
+              </div>
               <div className="tooltip" data-tip="Submit the proof to a smart contract to verify it on-chain.">
                 <button
                   className="btn btn-primary w-full"
-                  disabled={!pcd || isMintingNFT}
+                  disabled={!verifiedBackend || verifiedOnChain}
                   onClick={async () => {
                     try {
                       await mintNFT();
@@ -133,9 +178,10 @@ const Home: NextPage = () => {
                       notification.error(`Error: ${e}`);
                       return;
                     }
+                    setVerifiedOnChain(true);
                   }}
                 >
-                  {isMintingNFT ? <span className="loading loading-spinner"></span> : "3. Verify (on-chain) and mint"}
+                  {isMintingNFT ? <span className="loading loading-spinner"></span> : "4. Verify (on-chain) and mint"}
                 </button>
               </div>
               <div className="flex justify-center">
@@ -147,6 +193,9 @@ const Home: NextPage = () => {
                 >
                   Reset
                 </button>
+              </div>
+              <div className="text-center text-xl">
+                {yourBalance && yourBalance >= 1n ? "🎉 🍾 proof verified in contract!!! 🥂 🎊" : ""}
               </div>
             </div>
           </div>
